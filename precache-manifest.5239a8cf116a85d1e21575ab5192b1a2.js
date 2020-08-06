5cf4af298cc533fdff0318c7de6fbf5eaa881a14
Tramel Woodard <20341756+tramel-woodard@users.noreply.github.com>
2020-08-06 13:47:43 -0700

    Updates

---
+++ precache-manifest.5239a8cf116a85d1e21575ab5192b1a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95d0ddca53b335220e370235bd24d203",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "82c4c8786337b95ac222",
+    "url": "/monsters-rolodex/static/css/main.78cda383.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82c4c8786337b95ac222",
+    "url": "/monsters-rolodex/static/js/main.f9e4d1be.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
